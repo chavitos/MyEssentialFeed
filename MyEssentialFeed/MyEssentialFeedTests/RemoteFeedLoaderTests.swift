5a557a0d4e04bbdc99721c855b03ddd40909bfff
Tiago Chaves <tchaves74@gmail.com>
2023-12-11 17:55:18 -0300

    Simplify spy

--- MyEssentialFeed/MyEssentialFeedTests/RemoteFeedLoaderTests.swift
+++ MyEssentialFeed/MyEssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -13,16 +13,26 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
         
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLS.isEmpty)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLS, [url])
+    }
+    
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLS, [url, url])
     }
     
     //MARK: - Helpers
@@ -34,10 +44,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
+        var requestedURLS = [URL]()
         
         func get(from url: URL) {
-            requestedURL  = url
+            requestedURLS.append(url)
         }
     }
 }

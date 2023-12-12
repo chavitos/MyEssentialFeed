8f8e4040cab6239deb9d27d309eacf5617b00c99
Tiago Chaves <tchaves74@gmail.com>
2023-12-12 10:00:27 -0300

    Delivers empty items array on 200 HTTP response with JSON empty list

--- MyEssentialFeed/MyEssentialFeedTests/RemoteFeedLoaderTests.swift
+++ MyEssentialFeed/MyEssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -63,6 +63,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
+        
+        let emptyItemsJSON = Data("{\"Items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyItemsJSON)
+        
+        XCTAssertEqual(capturedResults, [.success([])])
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {

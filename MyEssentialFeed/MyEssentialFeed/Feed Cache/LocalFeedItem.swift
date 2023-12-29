dd89d6ac1819fc814d2ec407ebb0d0d4b7a61a56
Tiago Chaves <tchaves74@gmail.com>
2023-12-28 22:14:02 -0300

    Move `RemoteFeedItem` and `LocalFeedItem` to their own files

---
+++ MyEssentialFeed/MyEssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeedItem.swift
+//  MyEssentialFeed
+//
+//  Created by Tiago Chaves on 28/12/23.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}

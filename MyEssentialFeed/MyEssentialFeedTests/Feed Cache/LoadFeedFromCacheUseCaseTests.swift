4cc4bbbff8a2b36ec34aeb6d9001ad51e8adec6a
Tiago Chaves <tchaves74@gmail.com>
2023-12-31 15:56:37 -0300

    Load command does not delete cache when cache is already empty

--- MyEssentialFeed/MyEssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ MyEssentialFeed/MyEssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -68,13 +68,22 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         }
     }
     
-    func test_load_deletesCacheOnretrievalError() {
+    func test_load_deletesCacheOnRetrievalError() {
         let (sut, store) = makeSUT()
         
         sut.load { _ in }
         store.completeRetrieval(with: anyNSError())
         
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
+    func test_load_doesNotDeletesCacheOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        store.completeRetrievalWithEmptyCache()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {

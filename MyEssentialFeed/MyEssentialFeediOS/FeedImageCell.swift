ddbc17595784e98465474fdfc7c79a357a503fc3
Tiago Chaves <tchaves74@gmail.com>
2024-01-11 22:36:11 -0300

    Render loaded feed on successful load completion

---
+++ MyEssentialFeed/MyEssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  MyEssentialFeediOS
+//
+//  Created by Tiago Chaves on 11/01/24.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}

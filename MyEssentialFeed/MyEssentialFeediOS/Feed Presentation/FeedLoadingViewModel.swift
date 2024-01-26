5052e0d534c0020a236591371d744b43e871ebcc
Tiago Chaves <tchaves74@gmail.com>
2024-01-26 20:38:34 -0300

    Move ViewModels to separate files

---
+++ MyEssentialFeed/MyEssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  MyEssentialFeediOS
+//
+//  Created by Tiago Chaves on 26/01/24.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ MyEssentialFeed/MyEssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  MyEssentialFeediOS
+//
+//  Created by Tiago Chaves on 26/01/24.
+//
+
+import MyEssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}

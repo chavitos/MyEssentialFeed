dbd9780a1b6e91fbe90539474c35db5439b0a567
Tiago Chaves <tchaves74@gmail.com>
2024-02-19 22:02:45 -0300

    Unify FeedViewController static factory helper into the `FeedUIComposer`

--- MyEssentialFeed/MyEssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ MyEssentialFeed/MyEssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,21 +12,18 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader:
-                                                                    MainQueueDispatchDecorator(decoratee: feedLoader))
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader))
         
-        let feedController = FeedViewController.makeWith(delegate: presentationAdapter,
-                                                         title: FeedPresenter.title)
+        let feedController = makeWith(delegate: presentationAdapter,
+                                      title: FeedPresenter.title)
         
         presentationAdapter.presenter = FeedPresenter(feedView: FeedViewAdapter(controller: feedController,
                                                                                 imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
                                                       loadingView: WeakRefVirtualProxy(feedController))
         return feedController
     }
-}
-
-private extension FeedViewController {
-    static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+    
+    private static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
